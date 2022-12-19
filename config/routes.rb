3c530ba8477f4e41741b9005dfba3381f038d219
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-19 14:55:47 +0000

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
-  
-  resources :books, only: [:new, :create, :index, :show]
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
