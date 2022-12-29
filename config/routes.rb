cead8489cf226d130b748e4b416c68ee3c1fa153
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-29 08:08:35 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
+  get 'home/about' => 'homes#about', as: 'about'
 
   resources :books, only: [:new, :edit, :update, :create, :index, :show, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
